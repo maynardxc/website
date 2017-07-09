86da5cfb50c8d1256a3c902d9c21c7db009e2ad3
Fatty <fatty.arbuckle.87@gmail.com>
2017-07-08 23:29:46 -0400

    results

--- src/elm/Pages/Results.elm
+++ src/elm/Pages/Results.elm
@@ -19,8 +19,8 @@ type alias Model =
     }
 
 
-{ id, class, classList } =
-    Html.CssHelpers.withNamespace "ebws"
+-- { id, class, classList } =
+--     Html.CssHelpers.withNamespace "ebws"
 
 
 init : Model
@@ -36,4 +36,51 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-  text "results"
+  div []
+    [ table [ class "table table-striped" ]
+      [ thead [ class "thead-inverse" ]
+        [ tr []
+          [ th [] [ text "Date" ]
+          , th [] [ text "Location" ]
+          , th [ rowspan 4 ] [ text "Results" ]
+          ]
+        ]
+      , tbody []
+        [ resultRow "Nov. 12 2016" "District Meet" "" ""
+                    ""
+                    ""
+        , resultRow "Nov. 2 2016" "Hudson" "" ""
+                    "static/img/results/20161102 @ Hudson-Girls.jpg"
+                    "static/img/results/20161102 @ Hudson-Boys.jpg"
+        , resultRow "Oct. 25 2016" "Littleton" "" ""
+                    "static/img/results/20161025-Girls vs. Littleton @ Littleton.jpg"
+                    "static/img/results/20161025-Boys vs Littleton @ Littleton.jpg"
+        , resultRow "Oct. 13 2016" "Naragansett" "" ""
+                    "static/img/results/20161013Bromfield&Murdock @ Bromfield-Girls.jpg"
+                    "static/img/results/20161013-Bromfield&Murdock @ Bromfield-Boys.jpg"
+        , resultRow "Oct. 5 2016" "Naragansett" "" ""
+                    "static/img/results/20161005-Westford&AB @ Stonybrook-Girls.jpg"
+                    "static/img/results/20161005-Westford&AB @ Stonybrook-Boys.jpg"
+        , resultRow "Sept. 29 2016" "Naragansett" "" ""
+                    "static/img/results/2016 Girls naragansett & bromfield @ naragansett.jpg"
+                    "static/img/results/2016 Boys naragansett & bromfield @ naragansett.jpeg"
+        , resultRow "Sept. 22 2016" "Clinton" "" ""
+                    "static/img/results/20160922Murdock-Clinton @ Murdock - Girls.jpg"
+                    "static/img/results/20160922 Murdock-Clinton @ Murdock - Boys.jpg"
+        , resultRow "Sept. 9 2016" "Maynard" "" ""
+                    "static/img/results/2016 Sizer Charter - Girls.tif"
+                    "static/img/results/2016 Sizer Charter - Boys.tif"
+        ]
+      ]
+    ]
+
+resultRow : String -> String -> String -> String -> String -> String -> Html Msg
+resultRow date location points place girls boys =
+  tr []
+    [ td [] [ text date ]
+    , td [] [ text location ]
+    , td [] [ text points ]
+    , td [] [ text place ]
+    , img [ src girls, MainCss.styles [ Css.width (Css.px 30), Css.border3 (Css.px 4) Css.solid (Css.hex "#001212") ] ] []
+    , img [ src boys, MainCss.styles [ Css.width (Css.px 30), Css.border3 (Css.px 4) Css.solid (Css.hex "#001212") ] ] []
+    ]
