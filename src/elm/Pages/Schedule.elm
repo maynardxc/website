81361c0a5a32a1f6893ead196b600ab53a6a645c
Fatty <fatty.arbuckle.87@gmail.com>
2017-07-25 23:22:49 -0400

    updates

--- src/elm/Pages/Schedule.elm
+++ src/elm/Pages/Schedule.elm
@@ -26,4 +26,34 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-  text "This is what it is all a-schedule"
+  div []
+    [ table [ class "table table-striped" ]
+      [ thead [ class "thead-inverse" ]
+        [ tr []
+          [ th [] [ text "Date" ]
+          , th [] [ text "" ]
+          , th [] [ text "Team" ]
+          ]
+        ]
+      , tbody []
+        [ scheduleRow "Thursday Sept 7" "home" "Littleton"
+        , scheduleRow "Thursday Sept 14" "home" "Quabbin/Bromfield"
+        , scheduleRow "Monday Sept 18" "away" "Sizer(Saima Park)"
+        , scheduleRow "Thursday Sept 21" "away" "Tahanto"
+        , scheduleRow "Monday Sept 25" "home" "Hudson"
+        , scheduleRow "Thursday Sept 28" "away" "Invitational"
+        , scheduleRow "Thursday Oct. 5" "home" "Clinton"
+        , scheduleRow "Thursday Oct. 12" "home" "Ayer-Shirley"
+        , scheduleRow "Thursday Oct. 19" "home" "Murdock"
+        , scheduleRow "Saturday Oct. 28" "away" "League Meet"
+        ]
+      ]
+    ]
+
+scheduleRow : String -> String -> String -> Html Msg
+scheduleRow date ha team =
+  tr []
+    [ td [] [ text date ]
+    , td [] [ text ha ]
+    , td [] [ text team ]
+    ]
