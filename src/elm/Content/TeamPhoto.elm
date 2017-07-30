5cc70705f84025ae3259e1676a64517fc4e34e85
Fatty <fatty.arbuckle.87@gmail.com>
2017-07-29 23:21:49 -0400

    added caption to team photo

--- src/elm/Content/TeamPhoto.elm
+++ src/elm/Content/TeamPhoto.elm
@@ -11,11 +11,12 @@ card : Card.Config a
 card =
   Card.config [ Card.attrs [ class "photo" ] ]
     |> Card.block []
-      [ Card.text []
+      [ Card.text [ class "text-center" ]
         [ img
           [ src "static/img/2016_team.jpg"
           , alt "2016 team"
           , style [("max-width", "100%"), ("max-height", "100%")]
           ] []
+          , small [] [ text "The 2016 Maynard Cross Country Team" ]
         ]
       ]
